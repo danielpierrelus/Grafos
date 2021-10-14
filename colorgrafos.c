fd98e48a4c355a6d7ab893bb2eac368a35c9dba9
Daniel <pierrelusdaniel@gmail.com>
2021-10-14 18:45:28 -0300

    commit

--- colorgrafos.c
+++ colorgrafos.c
@@ -3,3 +3,21 @@
 int maiorGrauNColorido (int tabela[7][3]);
 void medeGrau (int mat[7][7], int tabela[7][3]);
 void printTabela (int tabela[7][3]);
+
+void coloreGrafo (int mat[7][7], int tabela[7][3]) {
+
+    medeGrau(mat, tabela);
+    for (int i; i < 7; i++){
+        i = maiorGrauNColorido(tabela);
+        tabela[i][1] = 1;
+        for (int j = 0; j < 7; j++){
+            if (mat[i][j] != 0) {
+                if (tabela[j][1] == tabela[i][1]){
+                    tabela[i][1]++;
+                    j = 0;
+                }
+            }
+        }
+    }
+    printTabela(tabela);
+}
